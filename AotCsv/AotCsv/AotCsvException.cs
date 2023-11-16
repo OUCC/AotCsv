52712db83bda501e109d6382fac482f450586c82
あいうえお <130837816+aiueo-1234@users.noreply.github.com>
2023-11-16 18:10:53 +0900

    SYSLIB0051の対処とtestにAOT-Analyzer追加

--- AotCsv/AotCsv/AotCsvException.cs
+++ AotCsv/AotCsv/AotCsvException.cs
@@ -4,15 +4,11 @@ using System.Runtime.CompilerServices;
 namespace Oucc.AotCsv;
 
 
-[Serializable]
 public class AotCsvException : Exception
 {
     public AotCsvException() { }
     public AotCsvException(string? message) : base(message) { }
     public AotCsvException(string? message, Exception? inner) : base(message, inner) { }
-    protected AotCsvException(
-      System.Runtime.Serialization.SerializationInfo info,
-      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 
     [MethodImpl(MethodImplOptions.NoInlining)]
     public static void ThrowMustNoQuoteException()
